bef02141db24ba8426fbc27c82f83bac46cbf48a
xiangormirko <xiangormirko@gmail.com>
2017-10-06 11:26:54 +0000

    gravatar and listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,27 @@ class UsersController < ApplicationController
   
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated succesfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+    
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  
+  
   private 
   def user_params
     params.require(:user).permit(:username, :email, :password)
